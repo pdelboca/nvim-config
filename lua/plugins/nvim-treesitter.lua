b69526cee70964f002a32b57274f4c08e17884b3
pdelboca <patriciodelboca@gmail.com>
2024-05-05 05:37:55 -0300

    Add terraform to treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return { -- Highlight, edit, and navigate code
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "bash", "html", "lua", "markdown", "vim", "vimdoc", "python" },
+      ensure_installed = { "bash", "html", "lua", "markdown", "vim", "vimdoc", "python", "css", "terraform" },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
