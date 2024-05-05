3ef62ac9350fd459fc1ce1642df134fcfedcffe6
pdelboca <patriciodelboca@gmail.com>
2024-05-05 04:59:37 -0300

    Tabs behaviour

--- init.lua
+++ init.lua
@@ -2,27 +2,22 @@ vim.g.mapleader = " "
 
 -- [[ Setting options ]]
 vim.cmd "colorscheme desert"
-
--- Disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
 vim.opt.number = true
 vim.opt.relativenumber=true
--- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
--- Sync clipboard between OS and Neovim.
---  See `:help 'clipboard'`
 vim.opt.clipboard = "unnamedplus"
 vim.opt.undofile = true
--- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
--- Sets how neovim will display certain whitespace in the editor.
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
--- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop =4
+vim.opt.shiftwidth = 4
 
 -- [[ Basic Keymaps ]]
 vim.keymap.set("n", "<leader>bd", "<cmd>%bd|e#|bd#<cr>", {desc = "Close all other buffers"} )
--- lua/plugins/bufdelete.lua
+++ lua/plugins/bufdelete.lua
@@ -1,1 +1,3 @@
-return {"famiu/bufdelete.nvim"}
+return {
+    "famiu/bufdelete.nvim"
+}
