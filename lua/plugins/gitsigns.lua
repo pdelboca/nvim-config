aafdb95e61aa512d2461320325b82b2cd16f27bc
pdelboca <patriciodelboca@gmail.com>
2024-05-06 10:49:49 +0200

    Improve gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,7 +4,8 @@ return {
     local gitsigns = require("gitsigns")
     gitsigns.setup({})
 
-    vim.keymap.set("n", "<leader>gd", gitsigns.preview_hunk_inline)
-    vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk)
+    vim.keymap.set("n", "<leader>gd", gitsigns.preview_hunk_inline, {desc = "[Git] Show hunk diff inline."} )
+    vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, {desc = "[Git] Reset current hunk"})
+    vim.keymap.set("n", "<leader>gb", gitsigns.blame_line, {desc = "[Git] Blame current line"})
   end
 }
