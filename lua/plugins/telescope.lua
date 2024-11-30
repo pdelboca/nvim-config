89b76169403c00663e23c2185a5be236b7b8fda1
pdelboca <patriciodelboca@gmail.com>
2024-11-30 09:38:27 +0100

    Map Ctrl-F to current_buffer_fuzzy_find

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,7 +38,7 @@ return {
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-      vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search in current buffer"})
+      vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search in current buffer"})
 			-- Overriding default of fuzzy search in current buffer
 			-- vim.keymap.set("n", "<leader>/", function()
 			-- 	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
