ad6604fffb582f32805ada44a76e90b09ce34d7d
pdelboca <patriciodelboca@gmail.com>
2024-11-30 10:31:17 +0100

    Simplify telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+
   "nvim-telescope/telescope.nvim",
   event = "VimEnter",
 		branch = "0.1.x",
@@ -28,24 +29,14 @@ return {
 			pcall(require("telescope").load_extension, "ui-select")
 			-- See `:help telescope.builtin`
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
       vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search in current buffer"})
-			-- Overriding default of fuzzy search in current buffer
-			-- vim.keymap.set("n", "<leader>/", function()
-			-- 	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-			-- 		winblend = 10,
-			-- 		previewer = false,
-			-- 	}))
-			-- end, { desc = "[/] Fuzzily search in current buffer" })
 		end,
 	}
 
