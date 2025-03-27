74209644ae916ec6c0da1ab782b920ef8559bde9
pdelboca <patriciodelboca@gmail.com>
2025-03-27 10:34:24 +0100

    Add clangd lsp and C syntax

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "pylsp", "ts_ls", "gopls" }
+        ensure_installed = { "pylsp", "ts_ls", "gopls", "clangd" }
       })
     end
   },
@@ -32,7 +32,7 @@ return {
       })
       lspconfig.ts_ls.setup({})
       lspconfig.gopls.setup({})
-
+      lspconfig.clangd.setup({})
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
