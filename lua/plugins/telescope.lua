1d6f48e111bc90613361c155c25e23e6aaa45f5b
Patricio Del Boca <patriciodelboca@gmail.com>
2025-11-13 15:23:20 -0300

    Add git_files to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,11 +20,6 @@ return {
       extensions = {
         ["ui-select"] = { require("telescope.themes").get_dropdown()}
       },
-      defaults = {
-        mappings = {
-          n = {["d"] = require("telescope.actions").delete_buffer},
-        },
-      },
     })
     -- Enable telescope extensions, if they are installed
     pcall(require("telescope").load_extension, "fzf")
@@ -35,6 +30,7 @@ return {
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>fo", builtin.git_files, { desc = "[S]earch by [G]it Files" })
     vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
     vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[S]earch [R]esume" })
     vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "[S]earch Document [S]ymbols" })
